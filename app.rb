7f34c60b3804228734f16d1695fbab4713dcafbf
Eloy Espinaco <eloyesp@gmail.com>
2016-12-12 12:20:29 -0300

    Formulario ingreso

--- app.rb
+++ app.rb
@@ -11,5 +11,18 @@ Cuba.define do
     on root do
       render('landing_page')
     end
+    on 'panel' do
+      if req.session['email']
+        res.write 'hola'
+      else
+        res.redirect('/ingreso')
+      end
+    end
+    on 'ingreso' do
+      render 'formulario_ingreso'
+    end
+    on 'debug' do
+      res.write req.session.to_hash.inspect
+    end
   end
 end
